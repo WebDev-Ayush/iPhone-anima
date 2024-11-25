627cacea70a528b88a6c688cd28bf82394d7d77a
Ayush Bisht <137602136+WebDev-Ayush@users.noreply.github.com>
2024-11-25 15:14:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
+   authToken: "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",
+    org: "tripie",
     project: "javascript-react"
   })],
 
